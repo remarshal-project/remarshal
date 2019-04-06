fde426af01e9627ad2a04feff3ea3427ae821dd0
dbohdan <dbohdan@dbohdan.com>
2019-04-06 11:25:39 +0300

    chore(remarshal): switch shebang to Python 3

--- remarshal.py
+++ remarshal.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # remarshal, a utility to convert between serialization formats.
 # Copyright (c) 2014, 2015, 2016, 2017, 2018, 2019 dbohdan
 # License: MIT
