f20db44e455ab373e21f4f74f4437d723c4bef92
D. Bohdan <dbohdan@dbohdan.com>
2026-01-17 15:17:26 +0000

    docs(license): update copyright

--- tests/test_remarshal.py
+++ tests/test_remarshal.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 # Remarshal, a utility to convert between serialization formats.
-# Copyright (c) 2014-2020, 2023-2025 D. Bohdan
+# Copyright (c) 2014-2020, 2023-2026 D. Bohdan
 # License: MIT
 
 from __future__ import annotations
